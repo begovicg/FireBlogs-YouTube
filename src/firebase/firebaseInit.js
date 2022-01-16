b33e75aa5038c40a3e60f8204387efc1ecd1006f
begovicg <begovicg@gmail.com>
2022-01-16 06:29:03 +0100

    Exporting firebase app

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBdDXIF5gDShrhRLjKvtqkXi3I0ImPrvj0",
     authDomain: "vueblogs-4422c.firebaseapp.com",
@@ -7,3 +10,12 @@ const firebaseConfig = {
     appId: "1:459800791622:web:8526853026872f3057d847",
     measurementId: "G-6XTSJ6HQ8H"
   };
+
+  // Initialize Firebase App with provided config
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const analytics = getAnalytics(firebaseApp);
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export {timestamp};
+  export {analytics};
+  export default firebaseApp.firestore();
