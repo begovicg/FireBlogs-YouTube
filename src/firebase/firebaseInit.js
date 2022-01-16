a6ee34f3249539e065b21c5dbf2ef062fd8614f0
begovicg <begovicg@gmail.com>
2022-01-16 06:23:35 +0100

    Added Firebase config
    https://console.firebase.google.com/u/0/project/vueblogs-4422c/overview

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdDXIF5gDShrhRLjKvtqkXi3I0ImPrvj0",
+    authDomain: "vueblogs-4422c.firebaseapp.com",
+    projectId: "vueblogs-4422c",
+    storageBucket: "vueblogs-4422c.appspot.com",
+    messagingSenderId: "459800791622",
+    appId: "1:459800791622:web:8526853026872f3057d847",
+    measurementId: "G-6XTSJ6HQ8H"
+  };
